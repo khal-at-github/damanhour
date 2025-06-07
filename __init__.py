2e103d0d5c765d0e54eb83673dc2e0fc2b1e29a6
khal-at-github <123749356+khal-at-github@users.noreply.github.com>
2025-06-07 14:24:18 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@
 from .selectionSort import selection_sort
 from .insertionSort import insertion_sort
 from .binarySearch import binary_search
-__all__=["selection_sort","insertion_sort","binary_search"]
+from .millerRabin import prime_test
+__all__=["selection_sort","insertion_sort","binary_search","prime_test"]
